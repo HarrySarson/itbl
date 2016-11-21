32af63adb070ec49fba07878596a91e6c2b6e70c
Harry Sarson <harry.sarson@hotmail.co.uk>
2016-11-21 20:46:23 +0000

    added build scripts

---
+++ .markdown-doctest-setup.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = {
+  'globals': {
+    itbl: require('./itbl.js'),
+    _: require('lodash'),
+    context: {
+      moveTo: function() {},
+      lineTo: function() {},
+    }
+  },
+};
